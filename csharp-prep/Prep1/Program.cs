533cb8406ffe9a80e8e901ca9b51a43bce4b04ad
epuyao-ruben <ruben.epuyao@gmail.com>
2024-01-13 14:54:24 -0300

    C# Prep 1 commit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
 }
