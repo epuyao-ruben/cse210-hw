2bae8db68e25a941ee1ace8f7f93869874f26c71
epuyao-ruben <ruben.epuyao@gmail.com>
2024-01-14 00:43:04 -0300

    Prep 4 Commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Creating list
+        List<int> numbers = new List<int>();
+
+        //initial value for usernumber and run the loop
+        int userNumber = -1;
+
+        while(userNumber != 0)
+        {
+            Console.Write("Enter a number, type 0 when finished. ");
+
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        //Sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+        //average
+        //number is float, so we need change sum type data
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //largest value
+        int max = numbers[0];
+
+        //found in every value and keep the largest
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
