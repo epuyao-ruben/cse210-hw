63cbc10b51b0218f8f12d7f00931ce7bd381f120
epuyao-ruben <ruben.epuyao@gmail.com>
2024-01-14 00:42:49 -0300

    Prep 2 Commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is Your Grade? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your letter is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You pass the class!");
+        }
+        else 
+        {
+            Console.WriteLine("You don't pass the class, success on your next time!");
+        }
     }
 }
