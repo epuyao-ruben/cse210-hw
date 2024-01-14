148574ddc0395c7b66e20cda9126b0f8a28cb035
epuyao-ruben <ruben.epuyao@gmail.com>
2024-01-14 00:42:57 -0300

    Prep 3 commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is your magic number? ");
+        //string userMagic = Console.ReadLine();
+        //int magicNumber = int.Parse(userMagic);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+        
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else{
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+
     }
 }
